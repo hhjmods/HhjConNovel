65efd5b8440b80f37f94989aa663c1a729576a07
hhjmods <hhjmods2@gmail.com>
2026-09-06 07:48:46 +0900

    Align run-end drop with app v17

--- src/story-con-run-end-drop.js
+++ src/story-con-run-end-drop.js
@@ -1,4 +1,4 @@
-import { applyStoryDropTransfer } from './app.js?v=20260906-16';
+import { applyStoryDropTransfer } from './app.js?v=20260906-17';
 import { storyAreaDropEffect } from './story-dnd-utils.js?v=20260906-2';
 
 const storyList = document.getElementById('storyList');
